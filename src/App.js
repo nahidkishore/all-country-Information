121c2e5f453d22ed57aa9c81c198ddaadbe6282e
nahid <nahidkishore99@gmail.com>
2020-08-25 19:40:15 +0600

    data load and  using useState and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [countries,setCountries]=useState([]);
+  useEffect(()=>{
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then(res=>res.json())
+    .then(data=> setCountries(data))
+    .catch(error=>console.log(error))
+  },[])
   return (
+    
     <div className="App">
+      <p>Country Loaded: {countries.length}</p>
+      <ul>
+        {
+          countries.map(country=>country.name)
+        }
+      </ul>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
